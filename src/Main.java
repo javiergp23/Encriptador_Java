39d7291062ef633964fbeb2f104aa9d7b383f44c
Javier Gonzalez <javgon2306@gmail.com>
2024-10-25 13:14:56 -0300

    add changes

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,40 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Que quieres haccer?");
+        System.out.println("1) Encriptar?");
+        System.out.println("2) Desencriptar");
+
+        Scanner teclado = new Scanner(System.in);
+        String opcionElegida = teclado.nextLine();
+
+        System.out.println("Ingrese la direccion del archivo");
+        String rutaDelArchivo = teclado.nextLine();
+
         Encriptador encriptador = new Encriptador();
         encriptador.setClave(432);
 
-        String texto = "Hola Mundo";
-        String textoEncriptado = encriptador.encriptar(texto);
-        System.out.println("Texto encriptado: " + textoEncriptado);
+        if("1".equals(opcionElegida)){
+            String texto = FileUtil.cargarArchivo(rutaDelArchivo);
+            String textoEncriptado = encriptador.encriptar(texto);
+            System.out.println("Texto encriptado: " + textoEncriptado);
+            String rutaArchivo = "";
+            FileUtil.guardarArchivo(rutaArchivo, textoEncriptado);
+        }else {
+            //String textoDesencriptado = encriptador.desencriptar(texto);
+            //System.out.println("texto desencriptado: " + textoDesencriptado);
+        }
 
-        texto = textoEncriptado;
-        String textoDesencriptado = encriptador.desencriptar(texto);
-        System.out.println("texto desencriptado: " + textoDesencriptado);
+        System.out.println("Proceso completado con Exito!");
+
+
+
+
+
+
     }
 }
